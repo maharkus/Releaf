3ef0b0a2ca110a26b42827bfb160adaed83ef4c9
Markus <essing-markus@web.de>
2022-01-21 18:43:53 +0100

    remove product.php

--- product.php
+++
@@ -1,7 +0,0 @@
-<div class="product">
-    <div class="imageWrap">
-        <img class="productImage" src="<?php echo $product->getImage() ?>" height="auto" width="100">
-    </div>
-    <h3><?php echo $product->name; ?></h3>
-    <p><?php echo $product->getPrice(); ?></p>
-</div>
