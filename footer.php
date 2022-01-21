090ea8132fa93d939fa8ab3c6384ec1b84cc57f8
Markus <essing-markus@web.de>
2022-01-21 16:37:55 +0100

    load content in index via php

---
+++ footer.php
@@ -0,0 +1,4 @@
+<footer>
+    <img class="waveTop" src="img/wave_top.svg" />
+    footer
+</footer>
--- index.php
+++ index.php
@@ -8,6 +8,7 @@
 </head>
 
 <body>
+    <!--Header-->
     <header>
         <img class="hero" src="img/header_image.jpg" title="HeaderImage" />
         <div class="logoWrap">
@@ -15,47 +16,24 @@
             <div class="spacerM"></div>
             <button>Shop Now</button>
         </div>
+
+        <!--Products Preview-->
         <div class="headerProducts">
             <h2>Tees für Körper und Seele</h2>
             <div class="productContainer">
-                <div class="product">
-                    <img class="productImage" src="" height="100" width="100">
-                    <h3>Tee</h3>
-                    <p>0,00€</p>
-                </div>
+                <?php
+                for ($i = 0; $i <= 3; $i++) {
+                    include "./product.php";
+                }
+                ?>
             </div>
         </div>
     </header>
-    <nav>
-        <img class="waveTop" src="img/wave_top.svg" />
-        <img class="logo" src="img/logo.png" title="Releaf" href="index.html" width="100" height="auto">
-        <ul class="navigation">
-            <li>
-                <a href="/shop" title="Zum Shop">Shop</a>
-            </li>
-            <li>
-                <a href="/zubehoer" title="Zubehör">Zubehör</a>
-            </li>
-            <li>
-                <a href="/unsere-werte" title="Zubehör">Unsere Werte</a>
-            </li>
-        </ul>
-        <ul class="icons">
-            <li>
-                <img src="img/icons/ic_search.svg">
-            </li>
-            <li>
-                <img src="img/icons/ic_heart.svg">
-            </li>
-            <li>
-                <img src="img/icons/ic_head.svg">
-            </li>
-            <li>
-                <img src="img/icons/ic_shopping_cart.svg">
-            </li>
-        </ul>
-        <img class="waveBottom" src="img/wave_bottom.svg" />
-    </nav>
+
+    <!--Navigation-->
+    <?php include "./nav.php"; ?>
+
+    <!--Main Content-->
     <main>
         <section>
             <div class="spacerL"></div>
@@ -69,10 +47,9 @@
             <div class="spacerL"></div>
         </section>
     </main>
-    <footer>
-        <img class="waveTop" src="img/wave_top.svg" />
-        footer
-    </footer>
+
+    <!--Footer-->
+    <?php include "./footer.php"; ?>
 </body>
 
 </html>
