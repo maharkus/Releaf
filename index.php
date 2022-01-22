376fe2666997a16e8e3f48361d8ce91d6101e98c
LucasOe <lucas.oelker@gmx.de>
2022-01-22 19:02:05 +0100

    Add header buttons

--- index.php
+++ index.php
@@ -24,6 +24,7 @@
             <div class="textbox">
                 <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergre. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invid</p>
             </div>
+            <button>Unsere Werte</button>
         </div>
 
         <!--Products Preview-->
@@ -40,6 +41,7 @@
                     </div>
                 <?php endforeach; ?>
             </div>
+            <button>Shop now</button>
         </div>
     </header>
 
