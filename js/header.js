5283b0e9bfb2a9434bbf8882659f814a551d6673
LucasOe <lucas.oelker@gmx.de>
2022-01-22 20:16:13 +0100

    Add action to scrollDown button

--- js/header.js
+++ js/header.js
@@ -1,17 +1,42 @@
-
-    $(document).ready(function () {
+$(document).ready(function () {
     var $window = $(window);
     $window.scroll(function () {
         var windowTop = $window.scrollTop();
 
-        var heroVal = -200 + windowTop/4 ;
-        var leafTransl = windowTop/2.5 ;
-        var leafRot = windowTop/20 ;
+        var heroVal = -200 + windowTop / 4;
+        var leafTransl = windowTop / 2.5;
+        var leafRot = windowTop / 20;
 
-
-        $('.hero').css("transform", "translateY(" + heroVal +  "px)");
-        $('.leaf').eq(0).css("transform", "translateY(" + leafTransl +  "px) rotate(" + leafRot + "deg)");
-        $('.leaf').eq(1).css("transform", "translateY(" + leafTransl +  "px) rotate(" + -leafRot + "deg) skew("+ -leafRot/2 + "deg)");
-        $('.leaf').eq(2).css("transform", "translateY(" + leafTransl +  "px) rotate(" + -leafRot + "deg) skew("+ -leafRot/2 + "deg)");
+        $(".hero").css("transform", "translateY(" + heroVal + "px)");
+        $(".leaf")
+            .eq(0)
+            .css(
+                "transform",
+                "translateY(" + leafTransl + "px) rotate(" + leafRot + "deg)"
+            );
+        $(".leaf")
+            .eq(1)
+            .css(
+                "transform",
+                "translateY(" +
+                    leafTransl +
+                    "px) rotate(" +
+                    -leafRot +
+                    "deg) skew(" +
+                    -leafRot / 2 +
+                    "deg)"
+            );
+        $(".leaf")
+            .eq(2)
+            .css(
+                "transform",
+                "translateY(" +
+                    leafTransl +
+                    "px) rotate(" +
+                    -leafRot +
+                    "deg) skew(" +
+                    -leafRot / 2 +
+                    "deg)"
+            );
     });
 });
