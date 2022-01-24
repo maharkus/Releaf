9e56bfe197551e8d9464d50ef8c0f3cebab3fda1
LucasOe <lucas.oelker@gmx.de>
2022-01-25 00:00:53 +0100

    Add more teas to database

--- db/releaf_category.sql
+++ db/releaf_category.sql
@@ -6,7 +6,7 @@ create table category
     image char(50) null
 );
 
-INSERT INTO releaf.category (id, name, image) VALUES (1, 'Konzentrationstee', 'tasse_000');
-INSERT INTO releaf.category (id, name, image) VALUES (2, 'Gesundheitstee', 'tasse_001');
-INSERT INTO releaf.category (id, name, image) VALUES (3, 'Entspannungstee', 'tasse_002');
-INSERT INTO releaf.category (id, name, image) VALUES (4, 'Früchtetee', 'tasse_003');
+INSERT INTO releaf.category (id, name, image) VALUES (1, 'Konzentrationstee', 'tasse_001');
+INSERT INTO releaf.category (id, name, image) VALUES (2, 'Gesundheitstee', 'tasse_002');
+INSERT INTO releaf.category (id, name, image) VALUES (3, 'Entspannungstee', 'tasse_003');
+INSERT INTO releaf.category (id, name, image) VALUES (4, 'Früchtetee', 'tasse_004');
