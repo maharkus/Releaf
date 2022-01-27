e1a4b1522ce2784fe4117bbcfc057b7c7e71d0b0
LucasOe <lucas.oelker@gmx.de>
2022-01-28 00:43:02 +0100

    Fix contact

--- contact.php
+++ contact.php
@@ -24,36 +24,17 @@
     <main id="contact">
         <div class="contact-wrapper">
             <h2>Kontakt</h2>
-            <p>
-                Du hast eine Frage?
-                Wir helfen Dir gerne!
-            </p>
+            <p>Du hast eine Frage? Wir helfen Dir gerne!</p>
             <form class="contact-form">
-
-                <!--Reverted input and label for adding styling to label depending on wether input is filled-->
-                <div class="input-group">
-                    <input type="text" id="name" name="firstname">
-                    <label for="name">Name</label>
-                </div>
-                <div class="input-group">
-                    <input type="email" id="email" name="email">
-                    <label for="email">E-Mail-Adresse</label>
-                </div>
-
-                <div class="input-group">
-                    <label for="country">Art der Anfrage</label>
-                    <select id="enquiry" name="type_of_enquiry">
-                        <option value="order_question">Frage zur Bestellung</option>
-                        <option value="product_range_question">Frage zum Sortiment</option>
-                        <option value="bug_report">Darstellungsfehler</option>
-                        <option value="other">Sonstiges</option>
-                    </select>
-                </div>
-
-                <div class="input-group">
-                    <label for="subject">Betreff</label>
-                    <textarea class="opensans" id="subject" name="subject" placeholder="Was möchtest du uns mitteilen?" style="height:200px"></textarea>
-                </div>
+                <input type="text" id="name" name="firstname" placeholder="Name">
+                <input type="email" id="email" name="email" placeholder="E-Mail-Adresse">
+                <select id="enquiry" name="type_of_enquiry">
+                    <option value="order_question">Frage zur Bestellung</option>
+                    <option value="product_range_question">Frage zum Sortiment</option>
+                    <option value="bug_report">Darstellungsfehler</option>
+                    <option value="other">Sonstiges</option>
+                </select>
+                <textarea class="opensans" id="subject" name="subject" placeholder="Was möchtest du uns mitteilen?" style="height:200px"></textarea>
                 <button class="button" type="submit" value="Submit">Abschicken</button>
 
             </form>
