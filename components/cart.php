f8cc870c640616793ed80815d9c8ae5834ebb712
LucasOe <lucas.oelker@gmx.de>
2022-01-27 21:13:48 +0100

    Fix empty cart

--- components/cart.php
+++ components/cart.php
@@ -2,10 +2,14 @@
 
 <div class="cartContainer">
     <h1 class="title">Warenkorb</h1>
-    <?php foreach ($_POST["data"] as $key => $element) : ?>
-        <?php $product = getProduct($element["id"]); ?>
-        <?php $amount = $element["amount"] ?>
-        <h1><?php echo $product->name ?></h1>
-        <h1><?php echo $amount ?></h1>
-    <?php endforeach; ?>
+    <?php if (!empty($_POST["data"])) : ?>
+        <?php foreach ($_POST["data"] as $key => $element) : ?>
+            <?php $product = getProduct($element["id"]); ?>
+            <?php $amount = $element["amount"] ?>
+            <h1><?php echo $product->name ?></h1>
+            <h1><?php echo $amount ?></h1>
+        <?php endforeach; ?>
+    <?php else : ?>
+        <p>Keine Produkte auf der Liste</p>
+    <?php endif; ?>
 </div>
