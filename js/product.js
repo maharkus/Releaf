ff9c77ff2ce58d10f96c0a89d462a88ab949fe23
LucasOe <lucas.oelker@gmx.de>
2022-01-27 20:12:08 +0100

    Fix amount selection

--- js/product.js
+++ js/product.js
@@ -1,13 +1,11 @@
 var $num = $("#num");
 
 function handleMinus() {
-    console.log("Test");
     var currentVal = parseInt($num.val());
     if (currentVal > 0) $num.val(currentVal - 1);
 }
 
 function handlePlus() {
-    console.log("Test");
     var currentVal = parseInt($num.val());
     if (currentVal < 99) $num.val(currentVal + 1);
 }
