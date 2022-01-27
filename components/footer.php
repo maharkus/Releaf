f8483df5decec21239d4a413b8b440859ee30ccd
Titus Hellinge <titus@hellinge.com>
2022-01-27 14:00:50 +0100

    footer

--- components/footer.php
+++ components/footer.php
@@ -1,4 +1,31 @@
 <footer>
     <img class="wave top" src="img/wave_top.svg" />
-    <p> Footer </p>
+    <div class="footer-links">
+        <a href="">
+            Kontakt
+        </a>
+        <a href="">
+            Impressum
+        </a>
+        <a href="">
+            Datenschutz
+        </a>
+    </div>
+        <ul>
+            <li>
+                <a href="https://instagram.com/titushellinge">
+                    <img src="img/icons/instagram.svg" alt="instagramIcon">
+                </a>
+            </li>
+            <li>
+                <a href="https://twitter.com/_LucasOe_">
+                    <img src="img/icons/twitter.svg" alt="twitterIcon">
+                </a>
+            </li>
+            <li>
+                <a href="https://stackoverflow.com/users/3956676/maharkus">
+                    <img src="img/icons/facebook.svg" alt="facebookIcon">
+                </a>
+            </li>
+        </ul>
 </footer>
