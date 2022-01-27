7450ef6211752d631c352e8e1a081ec6b2018f36
LucasOe <lucas.oelker@gmx.de>
2022-01-27 14:15:42 +0100

    Add product page

--- product.php
+++ product.php
@@ -1,4 +1,5 @@
 <?php include "components/connection.php" ?>
+<?php $product = getProduct($_GET["id"]); ?>
 
 <!DOCTYPE html>
 <html>
@@ -17,10 +18,26 @@
     </div>
 
     <!--Main Content-->
-    <main>
-        <?php $product = getProduct($_GET["id"]); ?>
-        <h1> <?php echo $product->name; ?> </h1>
+    <main class="productSite">
+        <div class="product-wrapper">
+            <div class="productImage">
+                <h1 class="productName"> </h1>
+                <div class="image-wrapper">
+                    <img class="image" src="<?php echo $product->getImage() ?>">
+                </div>
+            </div>
+            <div class="productDescription">
+                <h1 class="productName"> <?php echo $product->name; ?> </h1>
+                <div class="productBody">
+                    <h3>Beschreibung:</h3>
+                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ut fugiat ad illo, placeat accusamus corrupti ipsa nisi adipisci, veniam voluptatum provident! Quae beatae facilis corporis esse ipsum quis veniam saepe!</p>
+                </div>
+            </div>
+        </div>
     </main>
+
+    <!--Footer-->
+    <?php include "components/footer.php"; ?>
 </body>
 
 </html>
