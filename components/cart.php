f6ddfa3e1b4cbb4697794ac1a43a18c2f06a5660
LucasOe <lucas.oelker@gmx.de>
2022-01-28 13:08:39 +0100

    Add message for empty cart

--- components/cart.php
+++ components/cart.php
@@ -1,15 +1,21 @@
 <?php include "connection.php" ?>
 
-<div class="cartContainer">
-    <h1 class="title">Warenkorb</h1>
-    <?php if (!empty($_POST["data"])) : ?>
+<h1 class="title">Warenkorb</h1>
+<?php if (!empty($_POST["data"])) : ?>
+    <div class="fullCart">
         <?php foreach ($_POST["data"] as $key => $element) : ?>
             <?php $product = getProduct($element["id"]); ?>
             <?php $amount = $element["amount"] ?>
             <h1><?php echo $product->name ?></h1>
             <h1><?php echo $amount ?></h1>
         <?php endforeach; ?>
-    <?php else : ?>
-        <p>Keine Produkte auf der Liste</p>
-    <?php endif; ?>
-</div>
+    </div>
+<?php else : ?>
+    <div class="emptyCart">
+        <div class="text-wrapper">
+            <p class="opensans">Du hast noch keine Produkte in deinem Warenkorb.</p>
+            <p class="opensans">Entdecke jetzt unser vielseitiges Angebot:</p>
+            <button class="button">Shop</button>
+        </div>
+    </div>
+<?php endif; ?>
