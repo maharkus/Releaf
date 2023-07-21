e350ac3aece27a948289c82ea418f063492a0ae1
LucasOe <lucas.oelker@gmx.de>
2023-07-21 21:35:08 +0200

    Remove static site

--- out/js/register.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function () {
-    let translateX = 670;
-    $("a.next").click(e =>{
-        translateX = translateX - 670;
-        $('.registration-wrapper').css("transform", "translateX(" + translateX + "px)");
-    });
-});
